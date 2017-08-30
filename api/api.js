080f28cf6bca155de437f0491a0b4aaa6e81fda5
Thomas <thomas.rudfoss@gmail.com>
2017-08-30 21:36:50 +0200

    Added hashing of passwords and shared utility

--- api/api.js
+++ api/api.js
@@ -4,6 +4,7 @@
 const express = require('express'); // The actual webserver we're using
 const cors = require('cors'); // Plugin for Express that enables Cross Origin Resource Sharing requests (CORS)
 const helmet = require('helmet'); // Plugin for Express that secures our server by removing and adjusting response headers
+const hash = require('../shared/utils');
 
 const app = express(); // Create a new Express server instance
 const port = 4321; // Define the listening port
@@ -12,8 +13,28 @@ const port = 4321; // Define the listening port
 app.use(cors());
 app.use(helmet());
 
+// A variable to store todos in memory
+const store = {};
 
 // Respond to requests
+
+app.get('/lists', (req, res) => {
+  res.json(Object.keys(store));
+});
+
+app.get('/lists/:identifier', (req, res) => {
+  const hashedIdentifier = hash(req.params.identifier);
+  const lists = store[hashedIdentifier];
+  if (!lists){
+    res.sendStatus(404);
+  }
+  res.json(lists);
+});
+app.post('/lists/:identifier', (req, res) => {
+  const hashedIdentifier = hash(req.params.identifier);
+  store[hashedIdentifier] = {};
+  res.json({ok: true});
+});
 
 // Final fallback route that runs if no other route handles the request
 app.get('*', (req, res) => {
--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import {observable, observableArray, applyBindings} from 'knockout';
+import {observable, observableArray, computed, applyBindings} from 'knockout';
+import LoginBox from './components/LoginBox';
 import TodoList from './components/TodoList';
 
 import './App.scss';
 
 class App{
   constructor(){
+    this.loginBox = new LoginBox();
+    this.isLoggedIn = observable(false);
+
     this.lists = observableArray();
     this.openList = observable();
 
+    // Create a computed observable that uses isLoggedIn to determine the correct window title.
+    this.windowTitle = computed(() => {
+      const loginState = this.isLoggedIn();
+      if (!loginState){
+        return 'Todo - Login';
+      }
+      return 'Todo - '+this.loginBox.username();
+    });
+    this.windowTitle.subscribe(newTitle => document.title = newTitle);
+    this.windowTitle.notifySubscribers(this.windowTitle()); // Force the title to be correct by notifying our own subscriber
+
     this.evts = {
       onNewListClick: (model, evt) => this.newList(),
-      onListClick: (model, evt) => this.selectOpenList(model, evt)
+      onListClick: (model, evt) => this.selectOpenList(model, evt),
+      onLoginSubmit: (model) => this.onLogin()
     };
+  }
+
+  get hashUserPass(){
+    return this.loginBox.hashCombo;
   }
 
   newList(model, evt){
@@ -25,6 +45,9 @@ class App{
       return;
     }
     this.openList(todoList);
+  }
+  onLogin(){
+    this.isLoggedIn(true);
   }
 
   removeTodoList(todoList, confirmDelete = true){
