d9a5d5dfc3c5257ec38195f6f6b928148bb87c13
Thomas <thomas.rudfoss@gmail.com>
2017-09-01 09:39:15 +0200

    Corrected extra ,

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         use: [{
-          loader:'style-loader',
+          loader:'style-loader'
         }, {
           loader: 'css-loader'
         }, {
